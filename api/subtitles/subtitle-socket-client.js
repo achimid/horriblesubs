3ff97c48bc092b41d61d7a0d17ecb9b1f8f1eb34
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-04-14 10:05:27 -0300

    ajustando env

--- api/subtitles/subtitle-socket-client.js
+++ api/subtitles/subtitle-socket-client.js
@@ -1,7 +1,7 @@
 const server_url = process.env.KEEP_UP != 'false' ? process.env.MKV_EXTRACT_API : ''
 const socket = require('socket.io-client')(server_url, { transports: ['websocket'] })
 
-socket.on('reconnect_attempt', () => { socket.io.opts.transports = ['polling', 'websocket']})
+socket.on('reconnect_attempt', () => { socket.io.opts.transports = ['websocket']})
 
 socket.on('connect', () => console.info('Socket conectado ao servidor'))
 
