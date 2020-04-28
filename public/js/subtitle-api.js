0426eda861af62e67d8a735676ed087166c1f975
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-04-28 12:20:41 -0300

    ajuste

--- public/js/subtitle-api.js
+++ public/js/subtitle-api.js
@@ -1,10 +1,12 @@
-const SERVER_URL = 'https://horriblesubs-community.herokuapp.com'
-// const SERVER_URL = 'http://localhost:9002'
+// const SERVER_URL = 'https://horriblesubs-community.herokuapp.com'
+const SERVER_URL = 'http://localhost:9002'
 const SUGGESTION_URL = SERVER_URL + '/api/v1/sugestion'
 
 const language = 'pt'
-let skip = 0
-let page = 0
+let skip = getWithExpiry('skip') || 0
+let page = getWithExpiry('page') || 0
+
+if (skip > 200) skip = 0
 
 const getUrlSuggestion = () => `${SUGGESTION_URL}?language=${language}&skip=${skip}&page=${page}`
 
@@ -17,7 +19,7 @@ const $form = document.querySelector('form')
 const $suggestionTextArea = document.querySelector('#suggestion')
 
 const getDivSuggestions = (d) => {
-    return d.sugestions.map((sug, index) => `
+    return (d.sugestions || []).map((sug, index) => `
         <div class="col-md-12 ftco-animate special-col">
             <div class="box p-2 px-3 bg-light d-flex">
                 <div class="icon mr-3">
@@ -41,11 +43,17 @@ const getDialoguesFromAPI = () => {
             if (json.dialogues.length == 0) {
                 page = 0
                 skip++
+
+                setWithExpiry('page', page, 3600000 * 2) // 2 horas
+                setWithExpiry('skip', skip, 3600000 * 2) // 2 horas
+
                 return getDialoguesFromAPI()
             }
 
             dialogues.push(...json.dialogues)
             page++
+            
+            setWithExpiry('page', page, 3600000 * 2) // 2 horas
         })
 }
 
@@ -82,8 +90,40 @@ const sendNewSuggestionToAPI = (id, sugestion) => {
 }
 
 
-
 getDialoguesFromAPI()
     .then(() => renderNextDialogue())    
 
 
+
+function setWithExpiry(key, value, ttl) {
+    const now = new Date()
+
+    // `item` is an object which contains the original value
+    // as well as the time when it's supposed to expire
+    const item = {
+        value: value,
+        expiry: now.getTime() + ttl
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key) {
+    const itemStr = localStorage.getItem(key)
+
+    // if the item doesn't exist, return null
+    if (!itemStr) {
+        return null
+    }
+
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+
+    // compare the expiry time of the item with the current time
+    if (now.getTime() > item.expiry) {
+        // If the item is expired, delete the item from storage
+        // and return null
+        localStorage.removeItem(key)
+        return null
+    }
+    return item.value
+}
